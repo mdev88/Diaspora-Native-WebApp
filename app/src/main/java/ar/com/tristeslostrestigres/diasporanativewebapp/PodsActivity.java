c9c84307e3f21acf084b7dd30f20caabbf0da699
Martin V <johngalt12@gmx.com>
2015-06-13 19:34:36 -0300

    Cleanup

--- app/src/main/java/ar/com/tristeslostrestigres/diasporanativewebapp/PodsActivity.java
+++ app/src/main/java/ar/com/tristeslostrestigres/diasporanativewebapp/PodsActivity.java
@@ -147,6 +147,22 @@ public class PodsActivity extends ActionBarActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setMessage("Are you sure you want to exit?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("NO", null)
+                .show();
+    }
+
+    @Override
     protected void onDestroy() {
         unregisterReceiver(br);
         super.onDestroy();
